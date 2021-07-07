a646cd0d6acf17b0277fd8b922f6cc78a516dd75
Sheika1404 <rosheika1404@gmail.com>
2021-07-07 12:44:50 +0000

    fixed look of card

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,20 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Card = () => {
+const Card = ({ number }) => {
 	return (
 		<div
-			className="card align-middle text-center mr-2"
+			className="card align-middle text-center mr-2 mt-5"
 			style={{ height: "12rem", width: "8rem", background: "black" }}>
-			<div
-				className="card-body"
-				style={{ color: "white", fontSize: "4rem" }}>
-				<h5 className="card-title">0</h5>
+			<div className="card-body" style={{ color: "white" }}>
+				<h5 className="card-title" style={{ fontSize: "8rem" }}>
+					{number ? number : 0}
+					{/*If number is true show number else show 0*/}
+				</h5>
 			</div>
 		</div>
 	);
 };
-// Card.propTypes ={
-// 	number=PropTypes.number
-// }
+
+Card.propTypes = {
+	number: PropTypes.number
+};
 export default Card;
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./card";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 
 //create your first component
 export function Home() {
+	const [num1, setNum1] = useState(0);
+	if (num1 === 10) {
+		setNum1(0);
+	}
+	useEffect(() => {
+		setInterval(() => {
+			setNum1(num1 => num1 + 1);
+		}, 1000);
+	}, []);
+
 	return (
 		<div className="container-fluid text-center mt-5">
 			<h2>Simple Counter</h2>
-			<div className="container-fluid row m-auto">
+			<div className="container-fluid row m-auto justify-content-center">
 				<Card />
 				<Card />
 				<Card />
 				<Card />
 				<Card />
 				<Card />
-				<Card />
+				<Card number={num1} />
 			</div>
 		</div>
 	);
